a7f7fda6d88e9e1d15a570b71127a2a88b2c2075
SteveIpartek <estebansuarezramos5@gmail.com>
2025-03-13 18:59:25 +0100

    version 2.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
- // Aquí tu código
+const catch1 = document.getElementById("lista");
+const catch2 = document.getElementById("agregar");
+const catch3 = document.getElementById("limpiar");
+
+
+catch2.addEventListener("click", function() {
+    const intro1 = prompt("Introduce el elemento que deseas agregar a la lista --->");
+
+    if (intro1 && intro1.trim().length > 0) { 
+        const agre1 = document.createElement("li");
+        agre1.innerHTML = intro1;
+        catch1.appendChild(agre1); 
+    }
+});
+
+catch3.addEventListener("click" , function(){location.reload();})
